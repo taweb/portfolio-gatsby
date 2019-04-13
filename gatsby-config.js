1ee313ad850d675f72e3795577208151b2fd69b5
Tim Atherton <taweb@github.com>
2019-04-14 00:45:41 +0100

    Rename header info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Tim Atherton Portfolio`,
+    description: `Portfolio Site using Gatsbyjs`,
+    author: `Tim Atherton`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
