bcb8704d88b31946c23ba46174357c6b404e1d39
Tim Atherton <taweb@github.com>
2019-10-05 01:31:26 +0100

    remove console logs

--- src/components/animation.js
+++ src/components/animation.js
@@ -2,12 +2,6 @@ import React, { PureComponent } from 'react';
 
 class Animation extends PureComponent {
     state = {  }
-    componentDidMount() {
-        console.log('ANIMATION COMPONENT MOUNT');
-    }
-    componentDidUpdate(prevProps, prevState) {
-        console.log('ANIMATION COMPONENT UPDATE');
-    }
     render() { 
         const children = this.props.children
         return ( 
