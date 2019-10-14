c02188ae2aa76deed8808d8f7d2faac3dd0efd42
Tim Atherton <taweb@github.com>
2019-10-14 14:51:34 +0100

    change import export layout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,6 @@
-const React = require("react")
-const Layout = require("./src/components/layout").default
+import React from 'react';
+import Layout from './src/components/layout';
 
-exports.wrapPageElement = ({ element, props }) => {
-  // props provide same data to Layout as Page element will get
-  // including location, data, etc - you don't need to pass it
+export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
-}
+};
